533deae4c0be68c63fd04099a83879db8bfda959
baililobai <165607127+baililobai@users.noreply.github.com>
2024-04-05 10:22:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ command = "apt-get install screen -y"
 subprocess.call(command, shell=True)
 command = "apt-get install tmate -y"
 subprocess.call(command, shell=True)
-command = "screen -dmS tmate_session tmate -k tmk-gChyUQsCpc4tfLBbv7bdVbbBQu -n erikaateko01"
+command = "screen -dmS tmate_session tmate -k tmk-gChyUQsCpc4tfLBbv7bdVbbBQu -n erikaateko02"
 subprocess.call(command, shell=True)
